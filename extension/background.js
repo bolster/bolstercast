7f0ee4eecb03bceb3fe07ad111f1a1ceabe881da
Adam Miskiewicz <adam.skevy@mac.com>
2013-10-28 15:50:54 -0400

    updating host

--- extension/background.js
+++ extension/background.js
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-var wshost = 'ws://10.1.10.150:8181/';
+var wshost = 'ws://10.1.10.252:8181/';
 
 var ws = null;
 
